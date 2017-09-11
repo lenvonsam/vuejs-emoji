723aa7e115a328acf0017be4b891b6e64b6c7531
sam <sam@thinkingsam.cn>
2017-09-11 19:25:20 +0800

    fix webpack build unexcept token bug

--- plugins/components/emoji.js
+++ plugins/components/emoji.js
@@ -1,6 +1,6 @@
 import data from './emoji-data.js'
 let emojiData = {}
-Object.values(data).forEach(item => {
+Object.values(data).forEach(function (item) {
   Object.assign(emojiData, item)
 })
 
@@ -14,7 +14,7 @@ Object.values(data).forEach(item => {
 
 export function emoji (value) {
   if (!value) return
-  Object.keys(emojiData).forEach(item => {
+  Object.keys(emojiData).forEach(function (item) {
     value = value.replace(new RegExp(item, 'g'), createIcon(item))
   })
   return value
@@ -23,5 +23,5 @@ export function emoji (value) {
 function createIcon (item) {
   const value = emojiData[item]
   const path = './static/emoji/'
-  return `<img src=${path}${value} width="16px" height="16px">`
+  return '<img src=' + path + value + ' width="16px" height="16px">'
 }
